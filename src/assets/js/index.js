e4df5364b506bd01b788a3492deff5f36450a365
Basile <basile.bargibant@icloud.com>
2025-12-15 20:16:28 +0100

    Un changement utile

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,7 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Lancement du DidierLauncher" },
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
