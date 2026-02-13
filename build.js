96c4dc6d21566daae3e62a69938b531456c6c716
Basile <basile.bargibant@icloud.com>
2026-02-13 16:49:08 +0100

    Mise à jour 0.3.5

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 const builder = require('electron-builder')
 const JavaScriptObfuscator = require('javascript-obfuscator');
+const nodeFetch = require('node-fetch')
 const png2icons = require('png2icons');
 const { Jimp, JimpMime } = require('jimp');
 
@@ -64,7 +65,7 @@ class Index {
                 generateUpdatesFilesForAllChannels: false,
                 appId: preductname,
                 productName: preductname,
-                copyright: `Copyright © 2020-${new Date().getFullYear()} Luuxis`,
+                copyright: 'Copyright © 2020-2024 Luuxis',
                 artifactName: "${productName}-${os}-${arch}.${ext}",
                 extraMetadata: { main: 'app/app.js' },
                 files: ["app/**/*", "package.json", "LICENSE.md"],
@@ -83,14 +84,10 @@ class Index {
                     releaseType: 'release',
                 }],
                 win: {
-                    icon: "./app/assets/images/icon/icon.ico",
+                    icon: "./app/assets/images/icon.ico",
                     target: [{
                         target: "nsis",
                         arch: "x64"
-                    },
-                    {
-                        target: "nsis",
-                        arch: "arm64"
                     }]
                 },
                 nsis: {
@@ -100,19 +97,20 @@ class Index {
                     runAfterFinish: true
                 },
                 mac: {
-                    icon: "./app/assets/images/icon/icon.icns",
+                    icon: "./app/assets/images/icon.icns",
                     category: "public.app-category.games",
                     identity: null,
                     hardenedRuntime: false,
                     gatekeeperAssess: false,
                     mergeASARs: true,
+                    singleArchFiles: "node_modules/sqlite3/**/*",
                     target: [{
                         target: "dmg",
-                        arch: "universal"
+                        arch: ["arm64"]
                     },
                     {
                         target: "zip",
-                        arch: "universal"
+                        arch: ["arm64"]
                     }]
                 },
                 dmg: {
@@ -125,7 +123,7 @@ class Index {
                     format: "ULFO"
                 },
                 linux: {
-                    icon: "./app/assets/images/icon/icon.png",
+                    icon: "./app/assets/images/icon.png",
                     target: [{
                         target: "AppImage",
                         arch: "x64"
@@ -152,13 +150,19 @@ class Index {
         return file;
     }
 
-    async iconSet() {
-        const buffer = fs.readFileSync('src/assets/images/icon/icon.png');
-        let image = await Jimp.read(buffer);
-        image = await image.resize({ w: 256, h: 256 }).getBuffer(JimpMime.png);
-        fs.writeFileSync("src/assets/images/icon/icon.icns", png2icons.createICNS(image, png2icons.BILINEAR, 0));
-        fs.writeFileSync("src/assets/images/icon/icon.ico", png2icons.createICO(image, png2icons.HERMITE, 0, false));
-        fs.writeFileSync("src/assets/images/icon/icon.png", image);
+    async iconSet(url) {
+        const response = await nodeFetch(url)
+        if (response.status == 200) {
+            const buffer = await response.buffer()
+            let image = await Jimp.read(buffer);
+            image = await image.resize({ w: 256, h: 256 }).getBuffer(JimpMime.png);
+            fs.writeFileSync("src/assets/images/icon.icns", png2icons.createICNS(image, png2icons.BILINEAR, 0));
+            fs.writeFileSync("src/assets/images/icon.ico", png2icons.createICO(image, png2icons.HERMITE, 0, false));
+            fs.writeFileSync("src/assets/images/icon.png", image);
+            // console.log('new icon set')
+        } else {
+            console.log('connection error')
+        }
     }
 }
 
